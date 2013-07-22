609f1203df75defc713b229b95210cd188e73bd3
Luke Brooker <mail@lukebrooker.com>
2013-07-22 13:51:05 +1000

    Fixing old styles and dropdown javascript

--- src/main/webapp/1/assets/js/components/dropdown.js
+++ src/main/webapp/1/assets/js/components/dropdown.js
@@ -11,9 +11,14 @@ $(function() {
     var dropdown = $(this).parent(".dropdown");
     $(".dropdown.is-active").not(dropdown).removeClass("is-active")
                             .parents(".dropdown__container").removeClass("is-active");
-    dropdown.toggleClass("is-active").parents(".dropdown__container");
+    dropdown.toggleClass("is-active").parents(".dropdown__container").toggleClass("is-active");
     e.stopPropagation();
   };
+
+  // Don't toggle dropdown when clicking links inside it
+  $(".dropdown__toggle a, .dropdown__content").bind("click", function(e) {
+    e.stopPropagation();
+  });
 
   $(document).bind("click", collapseActiveDropdowns);
   $(".dropdown__toggle").bind("click", toggleThisCollapseOthers);
