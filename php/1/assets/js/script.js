0050653d6de047f39dc9b299215edcdaa4a28aee
Luke Brooker <mail@lukebrooker.com>
2013-07-30 10:08:42 +1000

    Updated loader to be faster. Also removed return to initial state.

--- php/1/assets/js/script.js
+++ php/1/assets/js/script.js
@@ -2389,13 +2389,14 @@ $(function () {
   $(".loader").click(function() {
     if ($(".loader__spinner", this).length <= 0) {
       $(".loader__label", this).append("<span class='loader__spinner'><span></span><span></span><span></span></span>");
-      var loader = $(this).addClass("is-active");
-      setTimeout(function() {
-        loader.removeClass("is-active");
-        setTimeout(function() {
-          $(".loader__spinner", loader).remove();
-        }, 300);
-      }, 1200);
+      $(this).addClass("is-active");
+      // var loader = $(this).addClass("is-active");
+      // setTimeout(function() {
+      //   loader.removeClass("is-active");
+      //   setTimeout(function() {
+      //     $(".loader__spinner", loader).remove();
+      //   }, 300);
+      // }, 1200);
     }
   });
 });
