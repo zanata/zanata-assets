6b88f3c0d956bc5dd79bf68b0526e6d0d510de67
Alex Eng <aeng@redhat.com>
2013-08-13 11:15:57 +1000

    Update dashboard mockup page

--- php/1/assets/js/script.js
+++ php/1/assets/js/script.js
@@ -1,4 +1,4 @@
-/*! zanata-proto - v0.1.0 - 2013-08-12
+/*! zanata-proto - v0.1.0 - 2013-08-13
 * https://github.com/lukebrooker/zanata-proto
 * Copyright (c) 2013 Luke Brooker; Licensed MIT */
 /*! Hammer.JS - v1.0.5 - 2013-04-07
