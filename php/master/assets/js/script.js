7593bfe4f48c7f61480012fe0026a9685fe7e88d
Luke Brooker <mail@lukebrooker.com>
2013-09-27 12:19:49 +1000

    Added better form input warning

--- php/master/assets/js/script.js
+++ php/master/assets/js/script.js
@@ -1,4 +1,4 @@
-/*! zanata-proto - v0.1.0 - 2013-09-26
+/*! zanata-proto - v0.1.0 - 2013-09-27
 * https://github.com/lukebrooker/zanata-proto
 * Copyright (c) 2013 Luke Brooker; Licensed MIT */
 /*! Hammer.JS - v1.0.5 - 2013-04-07
