b35cebf98c047b9e2e5bbdd427417eef1aec547c
Luke Brooker <mail@lukebrooker.com>
2013-10-16 15:39:57 +1000

    Updated with an actual working orientation bug fix

--- php/2/assets/js/components/orientation-bug-fix.js
+++ php/2/assets/js/components/orientation-bug-fix.js
@@ -1,56 +1,12 @@
-/*! A fix for the iOS orientationchange zoom bug.
- Script by @scottjehl, rebound by @wilto.
- MIT / GPLv2 License.
-*/
-(function(w){
-
-  // This fix addresses an iOS bug, so return early if the UA claims it's something else.
-  var ua = navigator.userAgent;
-  if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && /OS [1-5]_[0-9_]* like Mac OS X/i.test(ua) && ua.indexOf( 'AppleWebKit' ) > -1 ) ){
-    return;
+ (function(doc) {
+  var metas = doc.querySelectorAll('meta[name="viewport"]'),
+      forEach = [].forEach;
+  function fixMetas(isFirstTime) {
+   var scales = isFirstTime === true ? ['1.0', '1.0'] : ['0.25', '1.6'];
+   forEach.call(metas, function(el) {
+    el.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+   });
   }
-
-    var doc = w.document;
-
-    if( !doc.querySelector ){ return; }
-
-    var meta = doc.querySelector( 'meta[name=viewport]' ),
-        initialContent = meta && meta.getAttribute( 'content' ),
-        disabledZoom = initialContent + ',maximum-scale=1',
-        enabledZoom = initialContent + ',maximum-scale=10',
-        enabled = true,
-    x, y, z, aig;
-
-    if( !meta ){ return; }
-
-    function restoreZoom(){
-        meta.setAttribute( 'content', enabledZoom );
-        enabled = true;
-    }
-
-    function disableZoom(){
-        meta.setAttribute( 'content', disabledZoom );
-        enabled = false;
-    }
-
-    function checkTilt( e ){
-    aig = e.accelerationIncludingGravity;
-    x = Math.abs( aig.x );
-    y = Math.abs( aig.y );
-    z = Math.abs( aig.z );
-
-    // If portrait orientation and in one of the danger zones
-        if( (!w.orientation || w.orientation === 180) && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ){
-      if( enabled ){
-        disableZoom();
-      }
-        }
-    else if( !enabled ){
-      restoreZoom();
-        }
-    }
-
-  w.addEventListener( 'orientationchange', restoreZoom, false );
-  w.addEventListener( 'devicemotion', checkTilt, false );
-
-})( this );
+  fixMetas(true);
+  doc.addEventListener('gesturestart', fixMetas, false);
+ }(document));
