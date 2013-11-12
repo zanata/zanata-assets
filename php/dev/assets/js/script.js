ec903002bb63d247388de5d712ad38f078f85920
Luke Brooker <mail@lukebrooker.com>
2013-11-12 17:03:28 +1000

    Added new group screen Added js-message-removable and new states

--- php/dev/assets/js/script.js
+++ php/dev/assets/js/script.js
@@ -2417,6 +2417,26 @@ $(function () {
   });
 });
 
+$(function () {
+  $(document).on('click', '.js-message-remove', function(e) {
+    var $this = $(this),
+        $parent = $this.parents('.message--removable');
+    e.preventDefault();
+    if($parent.hasClass('is-active')) {
+      $parent.removeClass('is-active');
+      setTimeout(function() {
+        $parent.remove();
+      }, 300);
+    }
+    else {
+      $parent.addClass('is-removed');
+      setTimeout(function() {
+        $parent.remove();
+      }, 300);
+    }
+  });
+});
+
 $(function() {
   var offCanvasToggle = $('[class^=off-canvas__toggle], .off-canvas__close');
 
