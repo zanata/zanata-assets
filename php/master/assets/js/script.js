4795814939753db870614aec4eb19873a6fd833d
Luke Brooker <mail@lukebrooker.com>
2013-12-20 16:27:58 +1000

    Additional changes to project and version pages Fixes to dropdown, autocomplete and search Added new icon font files

--- php/master/assets/js/script.js
+++ php/master/assets/js/script.js
@@ -1,4 +1,4 @@
-/*! zanata-assets - v0.1.0 - 2013-12-17
+/*! zanata-assets - v0.1.0 - 2013-12-20
 * https://github.com/lukebrooker/zanata-proto
 * Copyright (c) 2013 Red Hat; Licensed MIT */
 /*jslint browser:true, node:true*/
