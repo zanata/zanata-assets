ab29e925278cadbb66848d9ad49f7b7d2ddbee60
Luke Brooker <mail@lukebrooker.com>
2014-01-03 13:46:24 +1000

    Finishing panel search Updating general layout Finishing group and project page options

--- php/master/assets/js/components/reveal.js
+++ php/master/assets/js/components/reveal.js
@@ -1,18 +1,43 @@
 $(function() {
-  $('.js-reveal__click').on('click', function() {
+  $('.js-reveal__show').on('click', function() {
+    var $revealTarget = $($(this).attr('data-target')),
+        $revealTargetInput = $revealTarget.find('.js-reveal__target__input'),
+        $revealParent = $(this).parents('.js-reveal');
+    $(this).addClass('is-hidden');
+    $revealParent.addClass('is-active');
+    $revealTarget.toggleClass('is-active');
+    setTimeout(function() {
+      $revealTargetInput.focus();
+    }, 100);
+  });
+  $('.js-reveal__reset').on('click', function() {
     var $revealTarget = $($(this).attr('data-target')),
         $revealTargetInput = $revealTarget.find('.js-reveal__target__input');
-    $(this).toggleClass('is-active');
-    $revealTarget.toggleClass('is-active');
-    if ($revealTarget.hasClass('is-active') && $revealTargetInput) {
-      console.log('True');
-      setTimeout(function() {
-        $revealTargetInput.focus();
-      }, 100);
+    $revealTargetInput.val('').focus();
+    $(this).addClass('is-hidden');
+  });
+  $('.js-reveal__cancel').on('click', function() {
+    var $revealTarget = $($(this).attr('data-target')),
+        $revealTargetInput = $revealTarget.find('.js-reveal__target__input'),
+        $revealParent = $(this).parents('.js-reveal');
+    $revealTarget.removeClass('is-active');
+    $revealTargetInput.blur();
+    $revealTargetInput.val('');
+    $revealParent.find('.js-reveal__reset').addClass('is-hidden');
+    $revealParent.find('.js-reveal__show').removeClass('is-hidden').focus();
+  });
+  $('.js-reveal__target__input').on('keyup', function(e) {
+    var $revealParent = $(this).parents('.js-reveal'),
+        $revealReset = $revealParent.find('.js-reveal__reset'),
+        $revealCancel = $revealParent.find('.js-reveal__cancel');
+    if ($(this).val() !== '') {
+      $revealReset.removeClass('is-hidden');
     }
-    else if($revealTargetInput) {
-      $revealTargetInput.blur();
-      console.log('False');
+    else {
+      $revealReset.addClass('is-hidden');
+    }
+    if (e.keyCode === 27) {
+      $revealCancel.click();
     }
   });
 });
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,6 +1,6 @@
-/*! zanata-assets - v0.1.0 - 2013-12-24
+/*! zanata-assets - v0.1.0 - 2014-01-03
 * https://github.com/lukebrooker/zanata-proto
-* Copyright (c) 2013 Red Hat; Licensed MIT */
+* Copyright (c) 2014 Red Hat; Licensed MIT */
 /*jslint browser:true, node:true*/
 /*global define, Event, Node*/
 
@@ -1723,20 +1723,45 @@ $(function() {
 });
 
 $(function() {
-  $('.js-reveal__click').on('click', function() {
+  $('.js-reveal__show').on('click', function() {
+    var $revealTarget = $($(this).attr('data-target')),
+        $revealTargetInput = $revealTarget.find('.js-reveal__target__input'),
+        $revealParent = $(this).parents('.js-reveal');
+    $(this).addClass('is-hidden');
+    $revealParent.addClass('is-active');
+    $revealTarget.toggleClass('is-active');
+    setTimeout(function() {
+      $revealTargetInput.focus();
+    }, 100);
+  });
+  $('.js-reveal__reset').on('click', function() {
     var $revealTarget = $($(this).attr('data-target')),
         $revealTargetInput = $revealTarget.find('.js-reveal__target__input');
-    $(this).toggleClass('is-active');
-    $revealTarget.toggleClass('is-active');
-    if ($revealTarget.hasClass('is-active') && $revealTargetInput) {
-      console.log('True');
-      setTimeout(function() {
-        $revealTargetInput.focus();
-      }, 100);
+    $revealTargetInput.val('').focus();
+    $(this).addClass('is-hidden');
+  });
+  $('.js-reveal__cancel').on('click', function() {
+    var $revealTarget = $($(this).attr('data-target')),
+        $revealTargetInput = $revealTarget.find('.js-reveal__target__input'),
+        $revealParent = $(this).parents('.js-reveal');
+    $revealTarget.removeClass('is-active');
+    $revealTargetInput.blur();
+    $revealTargetInput.val('');
+    $revealParent.find('.js-reveal__reset').addClass('is-hidden');
+    $revealParent.find('.js-reveal__show').removeClass('is-hidden').focus();
+  });
+  $('.js-reveal__target__input').on('keyup', function(e) {
+    var $revealParent = $(this).parents('.js-reveal'),
+        $revealReset = $revealParent.find('.js-reveal__reset'),
+        $revealCancel = $revealParent.find('.js-reveal__cancel');
+    if ($(this).val() !== '') {
+      $revealReset.removeClass('is-hidden');
     }
-    else if($revealTargetInput) {
-      $revealTargetInput.blur();
-      console.log('False');
+    else {
+      $revealReset.addClass('is-hidden');
+    }
+    if (e.keyCode === 27) {
+      $revealCancel.click();
     }
   });
 });
