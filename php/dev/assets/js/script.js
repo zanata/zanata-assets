d71b2d96ea5bf99cbcadbc208d076d5276e48945
Luke Brooker <mail@lukebrooker.com>
2014-03-07 13:43:35 +1000

    Update active state on new group and project screens aka panel screens

--- php/dev/assets/js/script.js
+++ php/dev/assets/js/script.js
@@ -1,4 +1,4 @@
-/*! zanata-assets - v0.1.0 - 2014-03-06
+/*! zanata-assets - v0.1.0 - 2014-03-07
 * https://github.com/lukebrooker/zanata-proto
 * Copyright (c) 2014 Red Hat; Licensed MIT */
 /*jslint browser:true, node:true*/
