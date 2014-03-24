e010afaf6752ff0d7e10d3546f4da505f0ca038a
Luke Brooker <mail@lukebrooker.com>
2014-03-24 16:21:47 +1000

    Added settings to the dashboard prototype

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,4 +1,4 @@
-/*! zanata-assets - v0.1.0 - 2014-03-20
+/*! zanata-assets - v0.1.0 - 2014-03-24
 * https://github.com/lukebrooker/zanata-proto
 * Copyright (c) 2014 Red Hat; Licensed MIT */
 /*jslint browser:true, node:true*/
