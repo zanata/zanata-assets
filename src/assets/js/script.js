0123c11f4c9fc469a5b9a6ec57958d18095afe07
Luke Brooker <mail@lukebrooker.com>
2014-03-25 11:55:54 +1000

    Adapt message remove icon to adapt to it's parent and align properly

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,4 +1,4 @@
-/*! zanata-assets - v0.1.0 - 2014-03-24
+/*! zanata-assets - v0.1.0 - 2014-03-25
 * https://github.com/lukebrooker/zanata-proto
 * Copyright (c) 2014 Red Hat; Licensed MIT */
 /*jslint browser:true, node:true*/
