d803f4637e9eef810aeede97dfe849899f867450
Luke Brooker <mail@lukebrooker.com>
2014-03-31 17:11:32 +1000

    Tweak messages component and modularise Add new icon font Add new porject list prototype container

--- php/4/assets/js/components/messages.js
+++ php/4/assets/js/components/messages.js
@@ -1,20 +1,72 @@
-jQuery(function () {
-  'use strict';
-  jQuery(document).on('click', '.js-message-remove', function (e) {
-    var $this = jQuery(this),
-        $parent = $this.parents('.message--removable');
-    e.preventDefault();
-    if ($parent.hasClass('is-active')) {
-      $parent.removeClass('is-active');
+'use strict';
+
+zanata.createNS('zanata.messages');
+
+zanata.messages = (function ($) {
+
+  var hide = function (el, e) {
+    var $el = $(el);
+    if (e) e.preventDefault();
+    if ($el.hasClass('is-active')) {
+      $el.removeClass('is-active');
       setTimeout(function () {
-        $parent.remove();
+        $el.remove();
       }, 300);
     }
     else {
-      $parent.addClass('is-removed');
+      $el.addClass('is-removed');
       setTimeout(function () {
-        $parent.remove();
+        $el.remove();
       }, 300);
     }
-  });
+  };
+
+  var activate = function (el) {
+    $(el).addClass('is-active');
+    updatePosition(el);
+  };
+
+  var deactivate = function (el) {
+    $(el).removeClass('is-active');
+  };
+
+  var updatePosition = function (el, elPositionFromTop) {
+    var $el = $(el),
+        elPosFromTop = (typeof elPositionFromTop !== 'undefined') ?
+          elPositionFromTop : $el.offset().top;
+    // Stop negative values setting the position to fixed
+    if (elPosFromTop < 0) elPosFromTop = 0;
+
+    if ($(window).scrollTop() > elPosFromTop) {
+      $el.addClass('is-fixed');
+    } else {
+      $el.removeClass('is-fixed');
+    }
+  };
+
+  var init = function () {
+    var messageGlobalTop = $('.message--global').offset().top;
+
+    $(document).on('click', '.js-message-remove', function (e) {
+      var $el = $(this).parents('.message--removable');
+      hide($el, e);
+    });
+
+    $(window).scroll(function (){
+      updatePosition('.message--global', messageGlobalTop);
+    });
+  };
+
+  // public API
+  return {
+    init: init,
+    hide: hide,
+    activate: activate,
+    deactivate: deactivate
+  };
+
+})(jQuery);
+
+jQuery(function () {
+  zanata.messages.init();
 });
