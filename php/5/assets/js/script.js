34a7d425498b471f43a36d831790b8028b89492a
Luke Brooker <mail@lukebrooker.com>
2014-04-24 15:23:13 +1000

    Update panel__results to add a bottom modifier that sticks to the bottom

--- php/5/assets/js/script.js
+++ php/5/assets/js/script.js
@@ -1,4 +1,4 @@
-/*! zanata-assets - v0.1.0 - 2014-04-14
+/*! zanata-assets - v0.1.0 - 2014-04-24
 * https://github.com/lukebrooker/zanata-proto
 * Copyright (c) 2014 Red Hat; Licensed MIT */
 /*jslint browser:true, node:true*/
